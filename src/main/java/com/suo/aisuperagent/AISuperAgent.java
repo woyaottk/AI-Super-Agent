4165d0b01e0d47cd77196f07db6cabfcbc5b46fd
wsq <2645118890@qq.com>
2025-05-25 00:19:24 +0800

    init

---
+++ src/main/java/com/suo/aisuperagent/AISuperAgent.java
@@ -0,0 +1,13 @@
+package com.suo.aisuperagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AISuperAgent {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AISuperAgent.class, args);
+    }
+
+}
---
+++ src/test/java/com/suo/aisuperagent/AiTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suo.aisuperagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AISuperAgentTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
